b7aa277578e4ca64c6f21e9d2b8c08bc54e586f9
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-10-19 15:39:18 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login';
+import Search from './Pages/Search';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+import ProfileEdit from './Pages/ProfileEdit';
+import NotFound from './Pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route component={ NotFound } />
+
+          </Switch>
+        </BrowserRouter>
+
+      </>
+    );
   }
 }
 
